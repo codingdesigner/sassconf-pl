35dc506b91abfd78fadabe167a1721e41f73e56f
Mason Wendell <mason@thecodingdesigner.com>
2014-08-16 17:39:54 -0400

    working on parallax header

--- source/js/main.js
+++ source/js/main.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
   // $scene.parallax('calibrate', true, true);
   // $scene.parallax('invert', true, false);
   // $scene.parallax('updateLayers');
+  var s = skrollr.init({
+    easing: "cubic"
+  });
+  console.log(s);
 });
