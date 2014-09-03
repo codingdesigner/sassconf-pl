38d79d5b7ce63037dd256953c77698ca7fab45cb
Mason Wendell <mason@thecodingdesigner.com>
2014-09-03 00:30:23 -0400

    public compile

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,5 @@
 $(document).ready(function () {
   //////////////////
-  // Skrollr
-  var photo_wrapper = $('.photo-wrapper');
-  var s = skrollr.init({
-    //easing: "cubic",
-    constants: {
-      photo: photo_wrapper.height(),
-      photorow: photo_wrapper.height() * 0.3
-    },
-    render: function(data) {
-      //Debugging - Log the current scroll position.
-      //console.log(data.curTop);
-    }
-  });
-  //////////////////
-
-  //////////////////
   // LETTERING.JS
   $('.standout h1').lettering('words');
   //////////////////
